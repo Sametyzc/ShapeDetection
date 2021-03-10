e45024ba76a00108d5243ce70964b3d40480ed53
Klewolas <48567287+Klewolas@users.noreply.github.com>
2021-03-10 18:52:52 +0300

    Clear test file.

--- project/test.py
+++ project/test.py
@@ -17,21 +17,3 @@ mask = cv.bitwise_or(mask1, mask2)
 
 sd = ShapeDetection(mask,showWindows=True)
 ellipseList = sd.DetectCircle()
-
-for ellipse in ellipseList:
-    cv.circle(image, ellipse.CenterPoint.GetAsTuple(), 3,
-                                      (255,120,255), -1, cv.LINE_8)
-    print("---------")
-    print("Center Point :",ellipse.CenterPoint)
-    print("Area :",ellipse.Area)
-    print("LongRadius :",ellipse.LongRadius)
-    print("AverageRadius :",ellipse.ShortRadius)
-    print("RotationAngle :",ellipse.RotationAngle)
-    print("---------")
-
-print("Toplam: ", len(ellipseList))
-cv.imshow("result image",image)
-cv.waitKey(0)
-a = Point(10,10)
-b = Point(0,0)
-print("aci: ", b.GetAngle(a))
